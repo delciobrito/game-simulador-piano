deaa4a5a35aac2c9127c04ae378ba16ce443e357
Delcio Brito <delciobrito@codigoemais.com>
2023-11-15 00:29:22 -0300

    Mapeando teclas permitidas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,5 +1,7 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key")
 
+let mapedKeys = []
+
 let audio = new Audio("./src/tunes/a.wav")
 
 const playTune = (key) => {
@@ -16,8 +18,11 @@ const playTune = (key) => {
 
 pianoKeys.forEach((key) => {
     key.addEventListener('click', () => playTune(key.dataset.key))
+    mapedKeys.push(key.dataset.key)
 })
 
 document.addEventListener("keydown", (e) => {
-    playTune(e.key)
+    if(mapedKeys.includes(e.key)) {
+        playTune(e.key)
+    }
 })
